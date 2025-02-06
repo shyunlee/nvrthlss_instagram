fe164367ed427757f77c12f39a9ab7526c13bdad
shyunlee <shyunlee0909@gmail.com>
2025-02-05 21:15:29 -0600

    add referrerPolicy for google profile image

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,12 +1,17 @@
 type AvatarProps = {
-  image?: string | null
-}
+  image?: string | null;
+};
 
 export default function Avatar({ image }: AvatarProps) {
   return (
     <div className='w-9 h-9 rounded-full bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300 p-[0.1rem]'>
       {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img className='rounded-full' alt={'profile image' } src={image ??  undefined }/>
+      <img
+        className='rounded-full'
+        alt={'profile image'}
+        src={image ?? undefined}
+        referrerPolicy='no-referrer'
+      />
     </div>
-  )
-};
+  );
+}

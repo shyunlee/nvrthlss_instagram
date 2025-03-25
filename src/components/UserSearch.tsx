bfd2c041843d2fab065a616ff04dcbe20543ae91
shyunlee <shyunlee0909@gmail.com>
2025-03-24 23:51:01 -0500

    UI logic for UserSearch component

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -1,15 +1,29 @@
 'use client';
 
-import { useState } from "react";
+import { SearchResultUser } from "@/model/user";
+import { FormEvent, useState } from "react";
 import useSWR from "swr";
+import GridSpinner from "./ui/GridSpinner";
 
 export default function UserSearch() {
   const [keyword, setKeyword] = useState('')
-  const { data, isLoading, error } = useSWR(`/api/search/${keyword}`)
-
+  const { data: users, isLoading, error } = useSWR<SearchResultUser[]>(`/api/search/${keyword}`)
+  function onSubmit (e: FormEvent) {
+    e.preventDefault();
+  }
   return (
     <>
-        
+      <form onSubmit={onSubmit}>
+        <input type="text" autoFocus placeholder="Search users by username or name" value={keyword} onChange={(e) => setKeyword(e.target.value)} />
+      </form>
+      {error && <p>something is wrong - error</p>}
+      {isLoading && <GridSpinner />}
+      {!isLoading && !error && users?.length === 0 && <p>No users found</p> }
+      {<ul>{
+        users?.map((user) => <li key={user.username}>
+        {user.username}
+      </li>)
+        }</ul>}
     </>
   )
 };

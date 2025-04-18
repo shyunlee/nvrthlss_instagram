c039cc216b5442f8a13a530c1f474340f849843a
shyunlee <shyunlee0909@gmail.com>
2025-04-17 23:16:23 -0500

    implement form submit ui logic in new post

--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -4,8 +4,10 @@ import { AuthUser } from '@/model/user';
 import PostUserAvatar from './PostUserAvatar';
 import FilesIcon from './ui/icons/FilesIcon';
 import Button from './ui/Button';
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, useRef, useState } from 'react';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import GridSpinner from './ui/GridSpinner';
 
 type NewPostProps = {
   user: AuthUser;
@@ -14,6 +16,10 @@ type NewPostProps = {
 export default function NewPost({ user: { username, image } }: NewPostProps) {
   const [isDragging, setIsDragging] = useState(false);
   const [file, setFile] = useState<File>();
+  const [error, setError] = useState<string>();
+  const [isLoading, setIsLoading] = useState(false);
+  const textRef = useRef<HTMLTextAreaElement>(null);
+  const router = useRouter();
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
@@ -43,10 +49,43 @@ export default function NewPost({ user: { username, image } }: NewPostProps) {
       setFile(files[0]);
     }
   };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    if (!file) return;
+    setIsLoading(true);
+    const formData = new FormData();
+    formData.append('file', file)
+    formData.append('text', textRef.current?.value ?? '')
+
+    fetch('/api/posts', {
+      method: 'POST',
+      body: formData
+    }).then((res) => {
+      if (!res.ok) {
+        setError(`${res.status} ${res.statusText}`)
+        return;
+      }
+      router.push(`/user/${username}`)
+    })
+    .catch((err) => setError(err.toString()))
+    .finally(() => setIsLoading(false));
+  }
+
   return (
     <section className='w-full max-w-xl flex flex-col items-center m-6'>
+      {isLoading && (
+        <div className='absolute inset-0 z-20 bg-neutral-900/70 text-center pt-[20%]'>
+          <GridSpinner color={'skyblue'}/>
+        </div>
+      )}
+      {error && (
+        <p className='w-full bg-red-100 p-3 font-bold mb-4 text-center text-red-600'>
+          {error}
+        </p>
+      )}
       <PostUserAvatar username={username} userImage={image ?? ''} />
-      <form className='w-full flex flex-col mt-2'>
+      <form className='w-full flex flex-col mt-2' onSubmit={handleSubmit}>
         <input
           className='hidden'
           name='input'
@@ -89,6 +128,7 @@ export default function NewPost({ user: { username, image } }: NewPostProps) {
           required
           rows={10}
           placeholder='Write a caption...'
+          ref={textRef}
         />
         <Button text='Publish' onClick={() => {}} />
       </form>
